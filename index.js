7ade50c73fe9a5abdd40d174c05422a15b578d87
Joas Souza <joassouzasantos@gmail.com>
2020-11-17 10:23:47 -0300

    [feat] Create health check
    On this commit we create a health check for the editor service. This
    health check is useful to know when the editor is ready to receive
    requests. The main issue is that the editor minimizes the client assets
    on the first request. This is a problem because if a client does the
    request before the CSS file is created a file without content will be
    minimized. Then, all requests to this file afterwards will require the
    same file minimized.
    There is not a straight way to disable the minimization of some files on
    Etherpad. It's all or nothing!

---
+++ index.js
@@ -0,0 +1,13 @@
+var checker = require('./checker');
+
+exports.expressCreateServer = function(hook_name, args, callback) {
+  var thisChecker = checker.init();
+  args.app.get('/health', function(req, res) {
+    var isHealth = thisChecker.areAllChecksFullfilled();
+    if (isHealth) {
+      res.json({ code: 200, health: isHealth });
+    } else {
+      console.log({isHealth});
+    }
+  });
+};
